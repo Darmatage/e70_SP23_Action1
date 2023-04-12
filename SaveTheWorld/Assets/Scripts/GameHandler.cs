4649440f0ba7b790b30516e6a6c95cdc259ac95f
Chandler Kilpatrick <crskilpatrick807@gmail.com>
2023-04-12 13:56:28 -0700

    Created PauseMenu Prefab, added to MainMenu

--- SaveTheWorld/Assets/Scripts/GameHandler.cs
+++ SaveTheWorld/Assets/Scripts/GameHandler.cs
@@ -3,8 +3,20 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.UI; 
 using UnityEngine.SceneManagement; 
+using UnityEngine.Audio;
+
 
 public class GameHandler : MonoBehaviour { 
+
+
+      // These are for the Pause Menu
+      public static bool GameisPaused = false;
+      public GameObject pauseMenuUI;
+      public AudioMixer mixer;
+      public static float volumeLevel = 1.0f;
+      private Slider sliderVolumeCtrl;
+      // End PauseMenu
+
 
       private GameObject player;
       public static int playerHealth = 100;
@@ -21,7 +33,22 @@ public class GameHandler : MonoBehaviour {
 
       private string sceneName;
 
+      // Used for PauseMenu
+      void Awake (){ 
+            SetLevel (volumeLevel); 
+            GameObject sliderTemp = GameObject.FindWithTag("PauseMenuSlider"); 
+            if (sliderTemp != null){ 
+                        sliderVolumeCtrl = sliderTemp.GetComponent<Slider>(); 
+                        sliderVolumeCtrl.value = volumeLevel; 
+            }
+      }
+
+
       void Start(){
+            // Used for PauseMenu
+            pauseMenuUI.SetActive(false); 
+            GameisPaused = false;
+
             player = GameObject.FindWithTag("Player");
             sceneName = SceneManager.GetActiveScene().name;
             //if (sceneName=="MainMenu"){ //uncomment these two lines when the MainMenu exists
@@ -29,6 +56,38 @@ public class GameHandler : MonoBehaviour {
             //}
             updateStatsDisplay();
       }
+
+      // Used for PauseMenu
+      void Update (){
+            if (Input.GetKeyDown(KeyCode.Escape)){
+                  if (GameisPaused){
+                              Resume();
+                  }
+                  else{
+                              Pause();
+                  }
+            }
+      }
+
+      // Used for PauseMenu
+      void Pause(){
+            pauseMenuUI.SetActive(true);
+            Time.timeScale = 0f;
+            GameisPaused = true;
+      }
+
+      // Used for PauseMenu
+      public void Resume(){
+            pauseMenuUI.SetActive(false);
+            Time.timeScale = 1f;
+            GameisPaused = false;
+      }
+
+      // Used for PauseMenu
+      public void SetLevel (float sliderValue){
+            mixer.SetFloat("MusicVolume", Mathf.Log10 (sliderValue) * 20); 
+            volumeLevel = sliderValue; 
+        } 
 
       public void playerGetTokens(int newTokens){
             gotTokens += newTokens;
@@ -83,6 +142,9 @@ public class GameHandler : MonoBehaviour {
       }
 
       public void RestartGame() {
+            // Used for PauseMenu
+            Time.timeScale = 1f; 
+
             SceneManager.LoadScene("MainMenu");
                 // Please also reset all static variables here, for new games!
             playerHealth = StartPlayerHealth;
