7f5b46ba0e114c872ec20787fe808176d37e79cf
lev603 <98661697+lev603@users.noreply.github.com>
2023-04-13 04:45:55 -0700

    I wrote the Zombie AI
    The Zombie now has a target it orbits around

--- SaveTheWorld/Assets/Scripts/ZombieMove.cs
+++ SaveTheWorld/Assets/Scripts/ZombieMove.cs
@@ -1,17 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class ZombieMove : MonoBehaviour
 {
     // Start is called before the first frame update
     public bool hunt;
-    public float target_X;
-    public float target_Y;
+    public float target_X = 0;
+    public float target_Y = 0;
 
     public float speed;
     public float attack;
     public float health;
+
+    public float orientation = 10;
+    public float num_orient = 360;
 
     void Start()
     {
@@ -21,8 +25,45 @@ public class ZombieMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(hunt) targeting();
-        tracking();
+        //Random rnd = new Random();
+        double x_calc = Math.Sin(Math.PI*orientation/num_orient);
+        double y_calc = Math.Cos(Math.PI*orientation/num_orient);
+
+        double distance = Math.Sqrt(Math.Pow(transform.position.x - target_X,2) + Math.Pow(transform.position.y - target_Y,2));
+
+        if(distance > 2)
+        {
+            hunt = true;
+        }
+
+        if(hunt)
+        {
+            double x = (target_X - transform.position.x)/distance;
+            double y = (target_Y - transform.position.y)/distance;
+            
+            //Vector3 target = new Vector3((float)x, (float)y, 0.0f);
+            //float angle = Vector3.Angle(target, transform.position);
+            if((y_calc > y + 0.1 || y_calc < y - 0.1)||(x_calc > x + 0.1 || x_calc < x - 0.1))
+                orientation++;
+            //orientation = (float)(Math.Asin(y));
+            speed = 5;
+            if(distance < 1)
+            {
+                hunt = false;
+            }
+        }
+        else
+        {
+            speed = 1;
+            //orientation ++;
+        }
+        Vector3 hvMove = new Vector3((float)x_calc, (float)y_calc, 0.0f);
+        transform.position = transform.position + hvMove * Time.deltaTime * speed;
+        transform.rotation = Quaternion.Euler(0, 0, (float)(-180*orientation/num_orient));
+
+
+        //if(hunt) targeting();
+        //tracking();
         
     }
 
