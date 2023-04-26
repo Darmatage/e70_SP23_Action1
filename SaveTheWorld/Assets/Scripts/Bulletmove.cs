7280bb5d374a781c15c300672c62bb4bc57c8ff8
Chandler Kilpatrick <crskilpatrick807@gmail.com>
2023-04-26 12:08:31 -0700

    Created Water Layer + made Bullet go over it.

--- SaveTheWorld/Assets/Scripts/Bulletmove.cs
+++ SaveTheWorld/Assets/Scripts/Bulletmove.cs
@@ -38,7 +38,7 @@ public class Bulletmove : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag != "CheckPoint" && collision.gameObject.tag != "Pickup") 
+        if (collision.gameObject.tag != "CheckPoint" && collision.gameObject.tag != "Pickup" && collision.gameObject.tag != "Water") 
         {
             Destroy(gameObject);
         }
