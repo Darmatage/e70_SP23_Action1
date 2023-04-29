cdd3300ebfcb1b28d335710c55063d240878c638
Darmatage <JasonWiserArt@gmail.com>
2023-04-29 01:56:27 -0400

    Added Helicopter system, but just to level 1
    Added script to JUST Level1 Player to manage helicopter system.
    Please play to see!

---
+++ SaveTheWorld/Assets/Scripts/Player_HelicopterHandler.cs
@@ -0,0 +1,109 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player_HelicopterHandler : MonoBehaviour{
+	//add this script to the player to manage the intro and outro copter sequences
+
+	public string NextLevel = "MainMenu";
+
+	private PlayerMoveAround playerMoveScript;
+	public GameObject playerArt;
+	public GameObject playerGunArt;
+	
+	public Animator animStartCoptor;
+	public Animator animEndCoptor;
+	
+	//startCopter
+	public GameObject startCopter;
+	public GameObject startC_ground; 
+	public GameObject startC_sky;
+	
+	//endCopter
+	public GameObject endCopter;
+	public GameObject endCopterSeat;
+	public GameObject endC_sky;
+	
+	//copter Management
+	public float moveSpeed = 1f;
+	public bool startCopterDown = false;
+	public bool startCopterUp = false;
+	public bool endCopterUp = false;
+	
+	//public AudioSource copterSoundFast;
+	//public AudioSource copterSoundSlow;
+	
+	void Awake(){
+		playerMoveScript = GetComponent<PlayerMoveAround>();
+	}
+	
+    void Start(){
+        //turn off the player (but keep it there for camera location 
+		playerMoveScript.enabled = false;
+		playerArt.SetActive(false);
+		playerGunArt.SetActive(false);
+		animEndCoptor.SetBool("slow", true);
+		StartCoroutine(StartCopter());
+		
+    }
+
+    void FixedUpdate(){
+        if (startCopterDown){
+			Vector2 posCopter = Vector2.Lerp ((Vector2)startCopter.transform.position, (Vector2)startC_ground.transform.position, moveSpeed * Time.fixedDeltaTime);
+            startCopter.transform.position = new Vector3 (posCopter.x, posCopter.y, transform.position.z);
+		}
+		else if (startCopterUp){
+			Vector2 posCopter = Vector2.Lerp ((Vector2)startCopter.transform.position, (Vector2)startC_sky.transform.position, moveSpeed * Time.fixedDeltaTime);
+            startCopter.transform.position = new Vector3 (posCopter.x, posCopter.y, transform.position.z);
+		}
+		else if (endCopterUp){
+			Vector2 posCopter = Vector2.Lerp ((Vector2)endCopter.transform.position, (Vector2)endC_sky.transform.position, moveSpeed * Time.fixedDeltaTime);
+            endCopter.transform.position = new Vector3 (posCopter.x, posCopter.y, transform.position.z);
+		}
+    }
+	
+	void OnTriggerEnter2D(Collider2D other){
+		//Activate EndCopter
+		if (other.gameObject.tag=="EndCopter"){
+			StartCoroutine(EndCopter());
+		}
+	}
+	
+	IEnumerator StartCopter(){
+		startCopterDown = true;
+		//copterSoundFast.Play();
+		yield return new WaitForSeconds(moveSpeed *2);
+		startCopterDown = false;
+		//copterSoundFast.Stop();
+		
+		//copterSoundSlow.Play();
+		animStartCoptor.SetBool("slow", true);
+		yield return new WaitForSeconds(2f);
+		playerMoveScript.enabled = true;
+		playerArt.SetActive(true);
+		playerGunArt.SetActive(true);
+		animStartCoptor.SetBool("slow", false);
+		//copterSoundSlow.Stop();
+		
+		startCopterUp = true;
+		//copterSoundFast.Play();
+		yield return new WaitForSeconds(moveSpeed *2);
+		startCopterUp = false;
+		//copterSound.Stop();
+		Destroy(startCopter);
+	}
+	
+
+	IEnumerator EndCopter(){
+		//copterSoundFast.Play();
+		playerMoveScript.enabled = false;
+		transform.position = endCopterSeat.transform.position;
+		transform.parent = endCopterSeat.transform;
+		animEndCoptor.SetBool("slow", false);
+		endCopterUp = true;
+		yield return new WaitForSeconds(moveSpeed *2);
+		SceneManager.LoadScene(NextLevel);
+	}
+	
+}
